f7ca1af1d49588a7920d6274339a148702bdd333
MoizCodeByte <hello@abdulmoiz.net>
2024-08-05 23:42:17 +0500

    Add error handling

--- Day #07 - Weather App/script.js
+++ Day #07 - Weather App/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiKey = 'your-api-key';
-    //generate api by going to this url sign in and copy the api key and paste there https://www.weatherapi.com/my/
+    const apiKey = '';
+      //generate api by going to this url sign in and copy the api key and paste there https://www.weatherapi.com/my/
+
     document.getElementById('search-form').addEventListener('submit', function(event) {
         event.preventDefault();
         const city = document.getElementById('city-input').value;
@@ -11,7 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
 
         fetch(apiUrl)
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('City not found');
+                }
+                return response.json();
+            })
             .then(data => {
                 console.log(data);
                 const location = data.location.name;
@@ -37,11 +43,21 @@ document.addEventListener('DOMContentLoaded', () => {
                 `;
                 document.getElementById('info').innerHTML = infoHTML;
 
-                // Show the weather data sections
+                // Show the weather data sections and hide error message
                 document.querySelector('.current-weather').style.display = 'block';
                 document.querySelector('.additional-info').style.display = 'block';
+                document.getElementById('error-message').style.display = 'none';
             })
-            .catch(error => console.error('Error fetching weather data:', error));
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+                // Show the error message
+                document.getElementById('error-message').textContent = 'City not found';
+                document.getElementById('error-message').style.display = 'block';
+
+                // Hide the weather data sections
+                document.querySelector('.current-weather').style.display = 'none';
+                document.querySelector('.additional-info').style.display = 'none';
+            });
     }
 
     // Fetch initial weather data for a default city
